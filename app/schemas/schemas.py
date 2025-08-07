d59cf423b5133160eb904b61c6a52e31c6fcf338
lucasberenger <thegreatberenger@gmail.com>
2025-08-07 14:10:41 -0300

    set model_config to post request schema

--- app/schemas/schemas.py
+++ app/schemas/schemas.py
@@ -1,6 +1,18 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from typing import Optional
 
 class ProductRequest(BaseModel):
+    id: Optional[int] = Field(description="Id is not needed on create", default=None)
     url: str
     telegram_chat_id: str
     target_price: float
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "url": "https://mercadolivre.com/produto",
+                "telegram_chat_id": "1234567890",
+                "target_price": 1099.9
+            }
+        }
+    }
